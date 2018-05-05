5023692542e20a6cb8c0c097afa6f73a26f6af1f
Giancarlo Saraceni <ghsaraceni@yahoo.com>
2018-05-05 08:34:22 -0700

    Now prints status to titlebar, more robust invalid path handling

--- Hashnamer/Program.cs
+++ Hashnamer/Program.cs
@@ -36,20 +36,35 @@ namespace Hashnamer {
 
 		//https://stackoverflow.com/questions/3670057/does-console-writeline-block
 		public static class NonBlockingConsole {
-			private static BlockingCollection<string> queue = new BlockingCollection<string>();
+			private static BlockingCollection<string> writeQueue = new BlockingCollection<string>();
+			private static BlockingCollection<string> titleQueue = new BlockingCollection<string>();
 
 			static NonBlockingConsole() {
-				var thread = new Thread(
-					() => {	while(true) Console.WriteLine(queue.Take());
+				var writeThread = new Thread(
+					() => {	while(true) {Console.WriteLine(writeQueue.Take());};
 					}) {
 					IsBackground = true
 				};
-				thread.Start();
+
+				var titleThread = new Thread(
+					() => {
+						while(true) {Console.Title = titleQueue.Take();};
+					}) {
+					IsBackground = true
+				};
+
+				writeThread.Start();
+				titleThread.Start();
 			}
 
 			public static void WriteLine(string value) {
-				queue.Add(value);
+				writeQueue.Add(value);
 			}
+			
+			public static string Title {
+				set { titleQueue.Add(value); }
+			}
+
 		}
 
 		static void RenameFileToHashname(string filename, IntHash hash, Dictionary<string, IntHash> filenamesToFileHash) {
@@ -59,59 +74,105 @@ namespace Hashnamer {
 					RenameFileToHashname(hashname, filenamesToFileHash[hashname], filenamesToFileHash);
 				}
 				File.Move(filename, hashname);
-				NonBlockingConsole.WriteLine(String.Format("Renamed {0} to {1}", filename, hashname));
+				NonBlockingConsole.Title = String.Format("Renamed {0} to {1}", filename, hashname);
 			} else {
-				NonBlockingConsole.WriteLine(String.Format("{0} left alone; already has a hashname", filename));
+				NonBlockingConsole.Title = String.Format("{0} left alone; already has a hashname", filename);
 			}
 		}
 
+		static void SantizeFolderPaths(ref List<string> folderPaths) {
+			folderPaths.RemoveAll((string folderPath) => {
+				try {
+					Path.GetFullPath(folderPath);
+				} catch {
+					Console.WriteLine(folderPath + " is not a valid path!");
+					return true;
+				}
+				if(!Directory.Exists(folderPath)) {
+					Console.WriteLine("Could not find " + folderPath);
+					return true;
+				}
+				if((File.GetAttributes(folderPath) & FileAttributes.Directory) != FileAttributes.Directory) {
+					Console.WriteLine(folderPath + " is not a folder!");
+					return true;
+				};
+				return false;
+			});
+		}
+
 		static void Main(string[] args) {
-			if(args.Length == 0) {
+			List<string> folderPaths = new List<string>(args);
+
+			SantizeFolderPaths(ref folderPaths);
+
+			if(folderPaths.Count == 0) {
 				Console.WriteLine("Please enter a path to a folder to work on:");
-				Array.Resize(ref args, 1);
-				args[0] = Console.ReadLine();
+				folderPaths.Add(Console.ReadLine().Trim('\"'));
+				SantizeFolderPaths(ref folderPaths);
 			}
 
-			for(int i = 0; i<args.Length; i++) {//work once on every path passed in
-				{
-					var attr = File.GetAttributes(args[i]);
-					if((attr & FileAttributes.Directory) == FileAttributes.Directory) {
-						NonBlockingConsole.WriteLine(args[i] + " is not a folder!");
-						break;
-					}
-				}
-				var filenames = Directory.EnumerateFiles(args[i]);
-				//probably faster to maintain this to check for hash collisions than checking the directory every time
-				var hashToOriginalFilenames = new Dictionary<IntHash, string>();
+			if(folderPaths.Count == 0) {
+				Console.WriteLine("Please enter a valid path to a folder to work on:");
+				folderPaths.Add(Console.ReadLine().Trim('\"'));
+				SantizeFolderPaths(ref folderPaths);
+			}
 
-				NonBlockingConsole.WriteLine(String.Format("Working on folder {0}...", args[i]));
+			if(folderPaths.Count == 0) {
+				Console.WriteLine("Exiting...");
+			} else {
+				foreach(string folderPath in folderPaths) {//work once on every path passed in
+					RenameFilesinFolderWithHash(folderPath);
+				}
+			}
+		}
 
-				foreach(string filename in filenames) {
-					var file = File.OpenRead(filename);
-					var md5Hash = new IntHash(MD5.Create().ComputeHash(file));
-					file.Close();
-					if(!hashToOriginalFilenames.ContainsKey(md5Hash)) {
-						hashToOriginalFilenames[md5Hash] = filename;
-					} else {//collision!
-						NonBlockingConsole.WriteLine(String.Format("Dupe detected! {0} hashed as a duplicate of {1} with an MD5 of {2}. Deleting duplicate...", filename, hashToOriginalFilenames[md5Hash], md5Hash));
-						FileSystem.DeleteFile(
-							filename,
-							UIOption.OnlyErrorDialogs,
-							RecycleOption.SendToRecycleBin,
-							UICancelOption.ThrowException
-						);
-					}
-				}
-				NonBlockingConsole.WriteLine("Dupes removed in " + args[i] + ", renaming files...");
-				var filenamesToFileHash = new Dictionary<string, IntHash>();
-				foreach(var hashAndFilename in hashToOriginalFilenames) {
-					filenamesToFileHash[hashAndFilename.Value] = hashAndFilename.Key;
-				}
-				foreach(var hashAndFilename in hashToOriginalFilenames) {
-					RenameFileToHashname(hashAndFilename.Value, hashAndFilename.Key, filenamesToFileHash);
-				}
+		private static void RenameFilesinFolderWithHash(string folderPath) {
+			var filenames = Directory.EnumerateFiles(folderPath);
+			//probably faster to maintain this to check for hash collisions than checking the directory every time
 
-				NonBlockingConsole.WriteLine("Done with "+args[i]);
+			NonBlockingConsole.WriteLine(String.Format("Working on folder {0}...", folderPath));
+
+			var hashToOriginalFilenames = HashFiles(new List<string>(filenames));
+
+			NonBlockingConsole.WriteLine("No dupes in " + folderPath + ", renaming files...");
+			var filenamesToFileHash = new Dictionary<string, IntHash>();
+			foreach(var hashAndFilename in hashToOriginalFilenames) {
+				filenamesToFileHash[hashAndFilename.Value] = hashAndFilename.Key;
+			}
+			foreach(var hashAndFilename in hashToOriginalFilenames) {
+				RenameFileToHashname(hashAndFilename.Value, hashAndFilename.Key, filenamesToFileHash);
+			}
+
+			NonBlockingConsole.WriteLine("Done with " + folderPath);
+		}
+
+		private static Dictionary<IntHash, string> HashFiles(List<string> filenames) {
+			var hashToOriginalFilenames = new Dictionary<IntHash, string>();
+
+			var fileCount = filenames.Count;
+
+			for(int i = 0; i<fileCount; i++) {
+				NonBlockingConsole.Title = String.Format("{0}% done, now processing {1}", i*100/fileCount, filenames[i]);
+				HashAndDedupeFile(ref hashToOriginalFilenames, filenames[i]);
+			}
+
+			return hashToOriginalFilenames;
+		}
+
+		private static void HashAndDedupeFile(ref Dictionary<IntHash, string> hashToOriginalFilenames, string filename) {
+			var file = File.OpenRead(filename);
+			var md5Hash = new IntHash(MD5.Create().ComputeHash(file));
+			file.Close();
+			if(!hashToOriginalFilenames.ContainsKey(md5Hash)) {
+				hashToOriginalFilenames[md5Hash] = filename;
+			} else {//collision!
+				NonBlockingConsole.WriteLine(String.Format("Dupe detected! {0} hashed as a duplicate of {1} with an MD5 of {2}. Deleting duplicate...", filename, hashToOriginalFilenames[md5Hash], md5Hash));
+				FileSystem.DeleteFile(
+					filename,
+					UIOption.OnlyErrorDialogs,
+					RecycleOption.SendToRecycleBin,
+					UICancelOption.ThrowException
+				);
 			}
 		}
 	}
